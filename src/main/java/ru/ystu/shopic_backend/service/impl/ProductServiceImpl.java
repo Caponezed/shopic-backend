9f6ccd74051bfc3776b6bd6f3d4a668bda1f830d
Anton Karpov <slam.caponezed@mail.ru>
2025-02-15 18:02:14 +0300

    product service impl init

---
+++ src/main/java/ru/ystu/shopic_backend/service/impl/ProductServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.ystu.shopic_backend.service.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.ystu.shopic_backend.dto.ProductDto;
+import ru.ystu.shopic_backend.entity.Product;
+import ru.ystu.shopic_backend.mapper.ProductMapper;
+import ru.ystu.shopic_backend.repository.ProductRepository;
+import ru.ystu.shopic_backend.service.ProductService;
+
+@Service
+@AllArgsConstructor
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Override
+    public ProductDto createProduct(ProductDto productDto) {
+        Product product = ProductMapper.mapToProduct(productDto);
+        Product savedProduct = productRepository.save(product);
+        return ProductMapper.mapToProductDto(savedProduct);
+    }
+}
