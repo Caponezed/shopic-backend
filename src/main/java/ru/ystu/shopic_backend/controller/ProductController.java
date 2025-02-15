e0e3a710be88ee300f57e23680911197a0eda335
Anton Karpov <slam.caponezed@mail.ru>
2025-02-15 18:23:45 +0300

    add get product by id method to controller

--- src/main/java/ru/ystu/shopic_backend/controller/ProductController.java
+++ src/main/java/ru/ystu/shopic_backend/controller/ProductController.java
@@ -3,10 +3,7 @@ package ru.ystu.shopic_backend.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.ystu.shopic_backend.dto.ProductDto;
 import ru.ystu.shopic_backend.service.ProductService;
 
@@ -21,5 +18,11 @@ public class ProductController {
     public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
         ProductDto savedProduct = productService.createProduct(productDto);
         return new ResponseEntity<ProductDto>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable("id") Long productId) {
+        ProductDto productDto = productService.getProductById(productId);
+        return ResponseEntity.ok(productDto);
     }
 }
