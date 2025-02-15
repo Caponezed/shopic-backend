eff0b1b6640546226dae027d641e1b324e90e5e6
Anton Karpov <slam.caponezed@mail.ru>
2025-02-15 17:51:31 +0300

    product dto model init

---
+++ src/main/java/ru/ystu/shopic_backend/dto/ProductDto.java
@@ -0,0 +1,18 @@
+package ru.ystu.shopic_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String type;
+    private Double price;
+    private String imgSrc;
+}
