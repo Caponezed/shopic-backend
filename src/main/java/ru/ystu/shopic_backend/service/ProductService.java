14694cbeca3b9ccdf2492460b0c93a642bb560d1
Anton Karpov <slam.caponezed@mail.ru>
2025-02-15 18:20:03 +0300

    add get product by id method

--- src/main/java/ru/ystu/shopic_backend/service/ProductService.java
+++ src/main/java/ru/ystu/shopic_backend/service/ProductService.java
@@ -4,4 +4,5 @@ import ru.ystu.shopic_backend.dto.ProductDto;
 
 public interface ProductService {
     ProductDto createProduct(ProductDto productDto);
+    ProductDto getProductById(Long productId);
 }
--- src/main/java/ru/ystu/shopic_backend/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/ystu/shopic_backend/service/impl/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.ystu.shopic_backend.dto.ProductDto;
 import ru.ystu.shopic_backend.entity.Product;
+import ru.ystu.shopic_backend.exception.ResourceNotFoundException;
 import ru.ystu.shopic_backend.mapper.ProductMapper;
 import ru.ystu.shopic_backend.repository.ProductRepository;
 import ru.ystu.shopic_backend.service.ProductService;
@@ -19,5 +20,13 @@ public class ProductServiceImpl implements ProductService {
         Product product = ProductMapper.mapToProduct(productDto);
         Product savedProduct = productRepository.save(product);
         return ProductMapper.mapToProductDto(savedProduct);
+    }
+
+    @Override
+    public ProductDto getProductById(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Товар не найден с id: " + productId));
+
+        return ProductMapper.mapToProductDto(product);
     }
 }
