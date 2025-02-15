dfe0e3a60db915e634ea2577275583944c44112f
Anton Karpov <slam.caponezed@mail.ru>
2025-02-15 18:01:59 +0300

    product service init

---
+++ src/main/java/ru/ystu/shopic_backend/service/ProductService.java
@@ -0,0 +1,7 @@
+package ru.ystu.shopic_backend.service;
+
+import ru.ystu.shopic_backend.dto.ProductDto;
+
+public interface ProductService {
+    ProductDto createProduct(ProductDto productDto);
+}
