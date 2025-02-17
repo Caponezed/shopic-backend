cc9673c1a77776332e8e9d3dce97847874c32b75
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 11:46:45 +0300

    add new exceptions for storage functionality

---
+++ src/main/java/ru/ystu/shopic_backend/exception/StorageException.java
@@ -0,0 +1,12 @@
+package ru.ystu.shopic_backend.exception;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ src/main/java/ru/ystu/shopic_backend/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package ru.ystu.shopic_backend.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
