0b29d08aad62e920e68e867cc94f669adfa8bbf7
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 11:49:11 +0300

    enable storage config (and disable cors with spring security temporarily)

--- src/main/java/ru/ystu/shopic_backend/ShopicBackendApplication.java
+++ src/main/java/ru/ystu/shopic_backend/ShopicBackendApplication.java
@@ -2,12 +2,15 @@ package ru.ystu.shopic_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.ystu.shopic_backend.config.StorageProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableConfigurationProperties(StorageProperties.class)
 public class ShopicBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopicBackendApplication.class, args);
 	}
-
 }
