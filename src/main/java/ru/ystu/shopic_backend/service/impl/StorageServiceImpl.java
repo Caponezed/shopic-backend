d8ea08d51129102ece4259f21c2758b4a62266ab
Anton Karpov <slam.caponezed@mail.ru>
2025-02-17 11:47:32 +0300

    implement storage service impl

---
+++ src/main/java/ru/ystu/shopic_backend/service/impl/StorageServiceImpl.java
@@ -0,0 +1,108 @@
+package ru.ystu.shopic_backend.service.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+import ru.ystu.shopic_backend.config.StorageProperties;
+import ru.ystu.shopic_backend.exception.StorageException;
+import ru.ystu.shopic_backend.exception.StorageFileNotFoundException;
+import ru.ystu.shopic_backend.service.StorageService;
+
+@Service
+public class StorageServiceImpl implements StorageService {
+	private final Path rootLocation;
+
+	@Autowired
+	public StorageServiceImpl(StorageProperties properties) {
+        if (properties.getLocation().trim().isEmpty())throw new StorageException("Нельзя сохранять файл, не указав путь к директории");
+		this.rootLocation = Paths.get(properties.getLocation());
+	}
+
+	@Override
+	public void store(MultipartFile file) {
+		try {
+			if (file.isEmpty()) {
+				throw new StorageException("Ошибка в процессе сохранения файла на сервер");
+			}
+
+			Path destinationFile = this.rootLocation.resolve(
+					Paths.get(Objects.requireNonNull(file.getOriginalFilename())))
+					.normalize().toAbsolutePath();
+
+			if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+				throw new StorageException("Нельзя сохранять файл не в целевую директорию");
+			}
+
+			try (InputStream inputStream = file.getInputStream()) {
+				Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+			}
+		}
+		catch (IOException e) {
+			throw new StorageException("Ошибка при сохранении файла на сервер", e);
+		}
+	}
+
+	@Override
+	public Stream<Path> loadAll() {
+		try {
+			return Files.walk(this.rootLocation, 1)
+				.filter(path -> !path.equals(this.rootLocation))
+				.map(this.rootLocation::relativize);
+		}
+		catch (IOException e) {
+			throw new StorageException("Ошибка при считывании файлов на сервере", e);
+		}
+	}
+
+	@Override
+	public Path load(String filename) {
+		return rootLocation.resolve(filename);
+	}
+
+	@Override
+	public Resource loadAsResource(String filename) {
+		try {
+			Path file = load(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			}
+			else {
+				throw new StorageFileNotFoundException("Не удалось считать файл на сервере: " + filename);
+			}
+		}
+		catch (MalformedURLException e) {
+			throw new StorageFileNotFoundException("Не удалось считать файл на сервере: " + filename, e);
+		}
+	}
+
+	@Override
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(rootLocation.toFile());
+	}
+
+	@Override
+	public void init() {
+		try {
+			Files.createDirectories(rootLocation);
+		}
+		catch (IOException e) {
+			throw new StorageException("Ошибка при создании директории для серверных файлов", e);
+		}
+	}
+}
