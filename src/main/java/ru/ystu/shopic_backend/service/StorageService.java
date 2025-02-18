acd26c3f97259e27b0ae386635963c999c83d808
Anton Karpov <slam.caponezed@mail.ru>
2025-02-18 15:05:21 +0300

    little cleanup

--- src/main/java/ru/ystu/shopic_backend/service/StorageService.java
+++ src/main/java/ru/ystu/shopic_backend/service/StorageService.java
@@ -7,17 +7,10 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public interface StorageService {
-
 	void init();
-
 	void store(MultipartFile file);
-
 	Stream<Path> loadAll();
-
 	Path load(String filename);
-
 	Resource loadAsResource(String filename);
-
 	void deleteAll();
-
 }
--- src/main/java/ru/ystu/shopic_backend/service/impl/StorageServiceImpl.java
+++ src/main/java/ru/ystu/shopic_backend/service/impl/StorageServiceImpl.java
@@ -7,8 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Objects;
 import java.util.stream.Stream;
 
